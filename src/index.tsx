d903ac0416668a837a241fc4c21e779519069c5d
vadimTimofeev <vadim.timofeev@autoeuro.ru>
2023-04-19 16:24:01 +0300

    init TS

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+console.log(ReactDOM, 'ReactDOM');
 
+// @ts-ignore
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
